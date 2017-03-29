e3d0d0cf6ad38c5fd6133f58df88d84dae3eeeca
Kevin Hathorn <hathornk@hotmail.com>
2017-03-29 23:17:18 +0000

    Added user_id to posts table, related posts to users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
 
 	def index
 		@posts = Post.all
@@ -9,14 +10,14 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		Post.create(post_params)
+		current_user.posts.create(post_params)
 		redirect_to root_path
 	end
 
 	private
 
 	def post_params
-		params.require(:post).permit(:title, :body, :author)
+		params.require(:post).permit(:title, :body)
 	end
 
 end
